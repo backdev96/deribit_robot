d0ff41e3ad2c6ddca5a8d257fa206a0bd25fdeee
Stas <stasefremovx@gmail.com>
2021-07-18 13:43:44 +0300

    Tydied

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
-from credentials import (client_id, client_secret, auth_url,
-                get_order_book_url,
-                cancel_orders_url, trade_url, get_open_orders_url)
+import json
 
 import requests
-import json
+
+from credentials import (auth_url, cancel_orders_url, client_id, client_secret,
+                         get_open_orders_url, get_order_book_url, trade_url)
+
 
 class Robot(object):
     def __init__(self, client_id, client_secret):
@@ -16,65 +17,48 @@ class Robot(object):
         self.trade_url = trade_url
         self.get_open_orders_url = get_open_orders_url
 
-
     def auth(self):
         context = {
-            'client_id' : self.client_id,
-            'client_secret' : self.client_secret,
-            'grant_type' : 'client_credentials',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'client_credentials',
         }
         response = self.session.get(self.auth_url, params=context)
         return json.loads(response.text)['result']['access_token']
 
-
     def request(self, url, context):
         auth_token = self.auth()
-        response = self.session.get(url, params=context, headers = {
-            'Accept':'application/json', 
+        response = self.session.get(url, params=context, headers={
+            'Accept': 'application/json',
             'Authorization': f'bearer {auth_token}'})
         return response
 
-
     def trade(self, instrument_name, amount, price, method):
         context = {
-            'instrument_name' : instrument_name,
-            'amount' : amount,
-            'price' : price
+            'instrument_name': instrument_name,
+            'amount': amount,
+            'price': price
         }
         if method == 'sell':
             return self.request(self.trade_url + method, context)
         elif method == 'buy':
             return self.request(self.trade_url + method, context)
 
-                
-
     def get_order_book(self, instrument_name, depth):
         context = {
-            'instrument_name' : instrument_name,
-            'depth' : depth
+            'instrument_name': instrument_name,
+            'depth': depth
         }
         return self.request(self.get_order_book_url, context)
-    
 
     def cancel_orders(self, typeDef='all'):
         return self.request(cancel_orders_url, {'type': typeDef})
-    
 
     def get_open_orders(self, instrument_name):
         context = {
-            'instrument_name' : instrument_name,
+            'instrument_name': instrument_name,
         }
         return self.request(self.get_open_orders_url, context)
 
-robot = Robot(client_id, client_secret)
 
-# robot.auth()
-# robot.cancel_orders()
-# robot.buy_order('BTC-PERPETUAL', 40, 'market', 'market0000234')
-# robot.sell_order(10, 'BTC-PERPETUAL', 145.61, 'last_price', 145)
-#robot.get_order_book('BTC-PERPETUAL', 5)
-# headers = {
-#             'Accept':'application/json', 
-#             'Authorization':  self.auth()
-#         }
-# self.request(self.private_buy_url, context)
+robot = Robot(client_id, client_secret)
--- robot.py
+++ robot.py
@@ -1,23 +1,29 @@
-from api import robot
-from api import *
 import time
+
 import yaml
+
+from api import *
+from api import robot
 
 
 def trade():
     """
     Описание алгоритма
-    1. Робот выставляет ордер номер 1 на покупку по цене buy price = current price - gap / 2.
-    (a) Если цена уменьшается до buy price, то ордер номер 1, скорее всего, будет исполнен. В этом
-    случае перейти к пункту 3.
-    (b) Если цена увеличивается до такого значения, что становится истинным условие current price
-    > buy price + gap + gap ignore, то робот должен отменить ордер номер 1. Далее, вернуться к пункту 1.
+    1. Робот выставляет ордер номер 1 на покупку по цене
+     buy price = current price - gap / 2.
+    (a) Если цена уменьшается до buy price, то ордер номер 1,
+     скорее всего, будет исполнен. В этом случае перейти к пункту 3.
+    (b) Если цена увеличивается до такого значения,
+     что становится истинным условие current price > buy price + gap + gap ignore,
+     то робот должен отменить ордер номер 1. Далее, вернуться к пункту 1.
     3. Робот выставляет ордер  на продажу по цене sell price = current price + gap.
-    4. (a) Если цена увеличивается до sell price, то ордер номер 2, скорее всего, будет исполнен. В этом
-    случае вернуться к пункту 1.
-    (b) Если цена уменьшается до такого значения, что становится истинным 
-    условие current price < sell price - gap - gap ignore, то робот должен отменить ордер номер 2. 
-    После этого следует вернуться к пункту 3.
+    4. (a) Если цена увеличивается до sell price,
+     то ордер номер 2, скорее всего, будет исполнен. В этом
+     случае вернуться к пункту 1.
+    (b) Если цена уменьшается до такого значения, что становится истинным
+     условие current price < sell price - gap - gap ignore,
+     то робот должен отменить ордер номер 2.
+     После этого следует вернуться к пункту 3.
     """
     method = 'buy'
     current_price = None
@@ -33,23 +39,25 @@ def trade():
 
         if method == 'buy':
             if orders_count == 0:
-            # Buy order creation.
+                # Buy order creation.
                 buy_price = current_price - gap/2
                 robot.trade(instrument_name, amount, buy_price, method)
                 orders_count += 1
                 print(f'Buy order created, buy price is {buy_price}')
                 method = 'sell'
             else:
-                print(f'Sell order located')
+                print('Sell order located')
+                # Sell order cancelling, completing.
                 if current_price < sell_price - gap - gap_ignore:
                     robot.cancel_orders()
                     print('Sell order canceled')
                     orders_count -= 1
-                    method = 'sell'         # Sell order cancelling, completing.
+                    method = 'sell'
                 elif sell_price > current_price:
                     method = 'buy'
                     orders_count = 0
-                    print(f'Sell order completed, current price is {current_price}')
+                    print('Sell order completed,'
+                          f'current price is {current_price}')
                 else:
                     print('waiting for sell_price < current_price')
 
@@ -62,18 +70,21 @@ def trade():
                 method = 'buy'
                 orders_count += 1
             else:
-                print(f'Buy order located')
+                print('Buy order located')
+                # Buy order cancelling, completing.
                 if buy_price > current_price:
-                    print(f'Buy order completed, current price is {current_price}')
+                    print('Buy order completed,'
+                          f'current price is {current_price}')
                     orders_count -= 1
                     method = 'sell'
                 elif current_price > buy_price + gap + gap_ignore:
                     robot.cancel_orders()
-                    print('Buy order canceled')     # Buy order cancelling, completing.
+                    print('Buy order canceled')
                     orders_count = 0
                     method = 'buy'
                 else:
                     print('waiting for buy_price < current_price')
+
 
 if __name__ == '__main__':
     # Import settings for bot.
@@ -81,7 +92,7 @@ if __name__ == '__main__':
         creds = yaml.full_load(file)
         gap = float(creds['gap'])
         gap_ignore = float(creds['gap_ignore'])
-        instrument_name =  creds['instrument_name']
+        instrument_name = creds['instrument_name']
         depth = int(creds['depth'])
         amount = int(creds['amount'])
 
